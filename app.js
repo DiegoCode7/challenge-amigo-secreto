96fccaebea7ae77a83eda6c6da18ad894eca9d76
DiegoCode7 <disenioinformatica@gmail.com>
2025-02-23 15:12:02 -0500

    Mi Challenge Amigo Secreto

---
+++ app.js
@@ -0,0 +1,45 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Programado por DIEGOFM
+
+// Array para almacenar los nombres de amigos
+let amigos = [];
+let ulListaAmigos = document.getElementById('listaAmigos');
+
+//Función para agregar un amigo
+function agregarAmigo(){
+    let nombreAmigos = document.getElementById('amigo').value;    
+    
+    if (nombreAmigos == '') {
+        alert('Por favor, ingrese un nombre válido');
+        document.getElementById("amigo").focus();
+    }
+    else if (amigos.includes(nombreAmigos)){
+        alert(`El nombre ${nombreAmigos} ya esta en la lista`);        
+    }
+    else {
+        amigos.push(nombreAmigos);        
+       //console.log(amigos);             
+        
+        ulListaAmigos.innerHTML += `<li> ${nombreAmigos} </li>`;
+        limpiarCaja();  
+        document.getElementById("amigo").focus();
+    }
+}
+
+//Función para sortear un amigo
+function sortearAmigo() {    
+    let indiceAmigos = Math.floor(Math.random()*amigos.length);
+    let amigoSorteado = amigos[indiceAmigos];
+    //console.log(indiceAmigos);
+
+    let ulResultado = document.getElementById('resultado');
+    ulResultado.innerHTML = `El amigo secreto sorteado es: <strong> ${amigoSorteado} </strong>`;
+    ulListaAmigos.innerHTML = '';
+    limpiarCaja(); 
+}
+
+//Función para limpiar la caja de texto
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';    
+}
+
